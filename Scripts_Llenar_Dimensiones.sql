f474ba4c985dc0afce55b052509e936f25c26748
ferAlvarado <fernandaalvaradov@hotmail.com>
2018-05-30 17:08:18 -0600

    Dimensiones: Pelicula,lugar y fecha

--- Scripts_Llenar_Dimensiones.sql
+++ Scripts_Llenar_Dimensiones.sql
@@ -14,5 +14,21 @@ AS $BODY$
 		 INNER JOIN FILM f ON f.film_id=fc.film_id
 		 INNER JOIN INVENTORY i ON i.film_id=f.film_id
 		 INNER JOIN RENTAL r ON i.inventory_id=r.inventory_id
+		GROUP BY ROLLUP category.name  film.title
+	
 END;
 $BODY$;
+
+ INSERT INTO DIMENSION_FECHA
+	SELECT extract(DAY FROM rental_date), extract(MONTH FROM rental_date), extract(YEAR FROM rental_date) 
+	FROM RENTAL
+	GROUP BY  extract(YEAR FROM renta_date), extract(MONTH FROM renta_date), extract(DAY FROM renta_date)
+	
+INSERT INTO DIMENSION_lUGAR
+	SELECT CITY.city, COUNTRY.country, STORE.store_id
+	FROM RENTAL r INNER JOIN STAFF s ON r.staff_id=s.staff_id
+		 INNER JOIN STORE st ON s.store_id=st.store_id
+		 INNER JOIN ADDRESS a ON st.address_id=a.address_id
+		 INNER JOIN CITY c  ON a.city_id=c.city_id
+		 INNER JOIN COUNTRY co ON co.country_id=c.country_id
+	GROUP BY CITY.city, COUNTRY.country, STORE.store_id
